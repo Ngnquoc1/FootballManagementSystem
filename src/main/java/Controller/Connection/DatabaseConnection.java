93fb4d83092bf6dabc80ce89c312104c00410909
Nguyen Nhu Quoc <nhuquoc1104@gmail.com>
2025-06-12 15:02:50 +0700

    fix DB

--- src/main/java/Controller/Connection/DatabaseConnection.java
+++ src/main/java/Controller/Connection/DatabaseConnection.java
@@ -24,8 +24,8 @@ public class DatabaseConnection {
     // Kết nối tới CSDL Oracle
     private void connectToDatabase() throws SQLException {
         final String url = "jdbc:oracle:thin:@localhost:1521:orcl"; // Địa chỉ CSDL
-        final String username = "c##2"; //  username thật
-        final String password = "2"; // g mật khẩu thật
+        final String username = "c##QLDB1"; //  username thật
+        final String password = "1"; // g mật khẩu thật
 
         try {
             // Load Oracle JDBC Driver (không bắt buộc với ojdbc 6 trở lên)
